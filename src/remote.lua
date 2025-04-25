21bbfa75b7f819e077e0bbff856853ca9170697b
Jim <xboss.sun@foxmail.com>
2025-04-26 00:25:51 +0800

    update

--- src/remote.lua
+++ src/remote.lua
@@ -26,6 +26,7 @@ if conf.key and type(conf.key) == "string" and #conf.key > 0 then
 end
 
 local recv_buf = ""
+local socks5_buf = ""
 local phase = 0
 local target_sock = nil
 local target_loop_thread = nil
@@ -71,7 +72,7 @@ local function target_loop()
         -- receive from target
         local raw, err = target_sock:receiveany(r_max_len)
         if not raw then
-            -- ngx.log(ngx.ERR, "Failed to receive target data ", err)
+            -- ngx.log(ngx.ERR, "Failed to receive target data", err)
             running = false
             break
         end
@@ -86,27 +87,32 @@ local function target_loop()
     target_loop_thread = nil
 end
 
-local function socks5(data) 
+local function socks5(socks5_buf) 
+    socks5_buf = socks5_buf .. socks5_buf
     ::socks5_continue::
-    while data and #data > 0 do
-        local data_len = #data
+    while socks5_buf and #socks5_buf > 0 do
+        local socks5_buf_len = #socks5_buf
         if phase == 0 then
             -- auth
             ngx.log(ngx.DEBUG, "auth start")
-            if #data < 3 or string.sub(data, 1, 1) ~= string.char(0x05) then
+            if #socks5_buf < 3 then
+                return true
+            end
+            if string.sub(socks5_buf, 1, 1) ~= string.char(0x05) then
                 send_socks5_resp(string.char(0x05) .. string.char(0x01))
                 return false
             end
+
             local bytes, err = send_socks5_resp(string.char(0x05) .. string.char(0x00))
             if bytes <= 0 then
                 ngx.log(ngx.ERR, "Failed to send socks5 ", err) 
                 return false
             end
-            if data_len > 3 then
-                data = string.sub(data, 4, data_len)
+            if socks5_buf_len > 3 then
+                socks5_buf = string.sub(socks5_buf, 4, socks5_buf_len)
             else 
-                assert(data_len == 3)
-                data = nil
+                assert(socks5_buf_len == 3)
+                socks5_buf = nil
             end
             phase = 1
             ngx.log(ngx.DEBUG, "auth end")
@@ -114,13 +120,16 @@ local function socks5(data)
         elseif phase == 1 then
             -- connect
             ngx.log(ngx.DEBUG, "connect start")
-            helper.print_hex("connect data ", data)
-            if #data < 7 or string.sub(data, 1, 1) ~= string.char(0x05) then
+            helper.print_hex("connect socks5_buf ", socks5_buf)
+            if #socks5_buf < 7 then
+                return true
+            end
+            if string.sub(socks5_buf, 1, 1) ~= string.char(0x05) then
                 ngx.log(ngx.DEBUG, "connect 111")
                 send_socks5_resp(string.char(0x05) .. string.char(0x01) .. string.char(0x00) .. string.char(0x01) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00))
                 return false
             end
-            local cmd = string.sub(data, 2, 2)
+            local cmd = string.sub(socks5_buf, 2, 2)
             if cmd ~= string.char(0x01) then
                 ngx.log(ngx.DEBUG, "connect 222 ")
                 send_socks5_resp(string.char(0x05) .. string.char(0x01) .. string.char(0x00) .. string.char(0x01) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00))
@@ -128,40 +137,41 @@ local function socks5(data)
             end
             local target_host = nil
             local addr_len = 0
-            local type = string.sub(data, 4, 4)
-            if type ~= string.char(0x01) then
+            local type = string.sub(socks5_buf, 4, 4)
+            if type == string.char(0x01) then
                 -- IP
                 ngx.log(ngx.DEBUG, "connect IP type")
-                if #data < 10 then 
+                if #socks5_buf < 10 then 
                     ngx.log(ngx.DEBUG, "connect 333")
                     send_socks5_resp(string.char(0x05) .. string.char(0x01) .. string.char(0x00) .. string.char(0x01) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00))
                     return false
                 end
-                local ip1, ip2, ip3, ip4 = string.byte(data, 5, 8)
+                local ip1, ip2, ip3, ip4 = string.byte(socks5_buf, 5, 8)
                 target_host = ip1 .. "." .. ip2 .. "." .. ip3 .. "." .. ip4
                 addr_len = 3
-            elseif type ~= string.char(0x03) then
+            elseif type == string.char(0x03) then
                 -- domain
                 ngx.log(ngx.DEBUG, "connect domain type")
-                addr_len = string.byte(data)
+                addr_len = string.byte(socks5_buf, 5, 5)
+                ngx.log(ngx.DEBUG, "connect domain type ", addr_len)
                 if addr_len <= 0 then
                     ngx.log(ngx.DEBUG, "connect 444")
                     send_socks5_resp(string.char(0x05) .. string.char(0x01) .. string.char(0x00) .. string.char(0x01) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00))
                     return false
                 end
-                if #data < 7 + addr_len then 
+                if #socks5_buf < 7 + addr_len then 
                     ngx.log(ngx.DEBUG, "connect 555")
                     send_socks5_resp(string.char(0x05) .. string.char(0x01) .. string.char(0x00) .. string.char(0x01) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00))
                     return false
                 end
-                target_host = string.sub(data, 2, addr_len + 1)
+                target_host = string.sub(socks5_buf, 6, addr_len + 6)
             else
                 ngx.log(ngx.DEBUG, "connect 666")
                 send_socks5_resp(string.char(0x05) .. string.char(0x01) .. string.char(0x00) .. string.char(0x01) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00) .. string.char(0x00))
                 return false
             end
 
-            local port1, port2 = string.byte(data, 6 + addr_len, 7 + addr_len) 
+            local port1, port2 = string.byte(socks5_buf, 6 + addr_len, 7 + addr_len) 
             local target_port = bor(lshift(port1, 8), port2)
             ngx.log(ngx.DEBUG, "target_host:", target_host, " target_port:", target_port)
 
@@ -183,24 +193,25 @@ local function socks5(data)
                 ngx.log(ngx.ERR, "Failed to send socks5 ", err) 
                 return false
             end
-            -- reset data
-            if data_len > 7 + addr_len then
-                data = string.sub(data, 8 + addr_len, data_len)
+            -- reset socks5_buf
+            if socks5_buf_len > 7 + addr_len then
+                socks5_buf = string.sub(socks5_buf, 8 + addr_len, socks5_buf_len)
             else 
-                assert(data_len == 7 + addr_len)
-                data = nil
+                assert(socks5_buf_len == 7 + addr_len)
+                socks5_buf = nil
             end
             phase = 2
             ngx.log(ngx.DEBUG, "connect end")
             goto socks5_continue
         elseif phase == 2 then
             -- data
-            local bytes, err = helper.pack_send(local_sock, data, aes_128_cbc)
-            if bytes < data_len then
-                data = string.sub(data, bytes + 1, data_len)
+            local bytes, err = helper.pack_send(local_sock, socks5_buf, aes_128_cbc)
+            if bytes < socks5_buf_len then
+                socks5_buf = string.sub(socks5_buf, bytes + 1, socks5_buf_len)
             else
-                data = nil
-                assert(data_len == bytes)
+                socks5_buf = nil
+                ngx.log(ngx.DEBUG, "connect data ", socks5_but_len, " ", bytes)
+                assert(socks5_buf_len == bytes)
             end
             phase = 3
             goto socks5_continue
@@ -216,7 +227,7 @@ local function local_loop()
         -- receive from local
         local raw, err = local_sock:receiveany(r_max_len)
         if not raw then
-            -- ngx.log(ngx.ERR, "Failed to receive local data ", err)
+            -- ngx.log(ngx.ERR, "Failed to receive local data", err)
             running = false
             break
         end
